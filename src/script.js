dd471593080321c7b913987e23d52d46ef0a447d
Justin <misterjustincox@gmail.com>
2018-12-20 12:37:39 -0500

    Import jaysQuery

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
+const classCache = {};
+
 function findDirection(e) {
   let direction = e.wheelDelta > 0 ? 'up' : 'down';
+  console.log(direction);
+}
+
+function changeNavbar(e) {
+  debugger
+  const nav = $j('.navbar');
+  // if (nav) {
+  //
+  // }
 }
 
 window.addEventListener("wheel", findDirection);
+window.addEventListener("resize", changeNavbar);
