12434b1c74be72927fe6c52767416e0adba4d5e1
Justin <misterjustincox@gmail.com>
2018-12-20 16:34:06 -0500

    Toggle dropdown meu

--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,5 @@
-let miniBar, navbar;
+let miniBar, navbar, hamburger, dropdownMenu;
+let dropdown = 'closed';
 
 const flip = (show, hide) => {
   show.show();
@@ -18,12 +19,26 @@ function changeNavbar(e) {
   }
 }
 
+function toggleDropdown(e) {
+  if (dropdown === 'closed') {
+    dropdownMenu.removeClass('hidden');
+    dropdown = "open";
+  } else {
+    dropdownMenu.addClass('hidden');
+    dropdown = "closed"
+  }
+}
+
 window.addEventListener("wheel", findDirection);
 window.addEventListener("resize", changeNavbar);
+
 
 document.addEventListener("DOMContentLoaded", (e) => {
   miniBar = $j('.navbar-reduced');
   navbar = $j('.navbar');
+  hamburger = $j('.hamburger-icon');
+  dropdownMenu = $j('.dropdown-links');
+  hamburger.on('click', toggleDropdown);
   if (navbar.width() < 800) {
     flip(miniBar, navbar);
   } else {
