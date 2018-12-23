254f2a018ec7ae81b4bec28b7afc59c29825bc8d
Justin <misterjustincox@gmail.com>
2018-12-23 07:29:09 -0500

    Create functional links on navbar

--- src/navbar.js
+++ src/navbar.js
@@ -10,10 +10,14 @@ Array.prototype.rotate = function(times = 1) {
 };
 
 let miniBar, navbar, hamburger, dropdownMenu, overlay, about, skills, portfolio,
-  education, contact, title, paragraph, lastChecked;
+  education, contact, title, paragraph, lastChecked, homeLinks, aboutLinks, skillsLinks,
+  portfolioLinks, educationLinks, contactLinks;
 
 let dropdown = 'closed';
 let transitioning = 'false';
+
+const homeTitle = "HOMEPAGE";
+const homePara = "Text here";
 
 const aboutMeTitle = "ABOUT ME";
 const aboutMePara = "I am a software engineer based in New York City with experience working with JavaScript, Ruby, Rails, React + Redux, SQL, HTML/CSS, and Git. My current hobbies include playing fighting games and puzzle solving. I fell in love with programming after a coworker noticed my passion for problem solving. This infatuation led me on a career path to become a software engineer. After a full year of studying, I was accepted into App Academy, a 1000-hour intensive coding bootcamp with an emphasis on test-driven development, scalability, object-oriented programming, coding style, and security. <br><br> The biggest draw to programming for me is having the ability to turn a blank text file into whatever you want from a stable and scalable algorithm to an elegant and functional landing page. I aim to, not only expand my knowledge of programming, but to use my knowledge and capabilities for the golbal benefit.";
@@ -64,27 +68,56 @@ function changeInfo(e) {
   // }
 }
 
+function changeTab(e) {
+  switch (e.target.innerText) {
+    case "HOME":
+      alterHtml(aboutMeTitle, aboutMePara);
+      return;
+    case "ABOUT":
+      alterHtml(aboutMeTitle, aboutMePara);
+      checkButton(about);
+      return;
+    case "SKILLS":
+      alterHtml(skillsTitle, skillsPara);
+      checkButton(skills);
+      return;
+    case "PORTFOLIO":
+      alterHtml(portfolioTitle, portfolioPara);
+      checkButton(portfolio);
+      return;
+    case "EDUCATION":
+      alterHtml(educationTitle, educationPara);
+      checkButton(education);
+      return;
+    case "CONTACT":
+      alterHtml(contactTitle, contactPara);
+      checkButton(contact);
+      return;
+    default:
+      return null;
+  }
+}
+
+function checkButton(button) {
+  button.nodes[0].checked = true;
+}
+
 function switchInfo(e) {
   switch (e.target.id) {
     case 'to-about':
       alterHtml(aboutMeTitle, aboutMePara);
-      lastChecked = about;
       return;
     case 'to-skills':
       alterHtml(skillsTitle, skillsPara);
-      lastChecked = skills;
       return;
     case 'to-portfolio':
       alterHtml(portfolioTitle, portfolioPara);
-      lastChecked = portfolio;
       return;
     case 'to-education':
       alterHtml(educationTitle, educationPara);
-      lastChecked = education;
       return;
     case 'to-contact':
       alterHtml(contactTitle, contactPara);
-      lastChecked = contact;
       return;
     default:
       return null;
@@ -113,11 +146,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
   infoScreens = $j('.info-screens');
   radioButtons = $j('.info-switch');
   about = $j('#to-about');
-  lastChecked = about;
   skills = $j('#to-skills');
   portfolio = $j('#to-portfolio');
   education = $j('#to-education');
   contact = $j('#to-contact');
+  links = $j('.section-link, .dropdown-link');
+  links.on('click', changeTab);
   title = $j('.info-header');
   paragraph = $j('.info-paragraph');
   radioButtons.on('click', switchInfo);
